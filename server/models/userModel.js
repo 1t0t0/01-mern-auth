fbea1808899b326ee77e8d3771ed5306ccc7ed82
Toto <toto@Totos-MacBook-Pro.local>
2024-12-27 16:13:46 +0700

    fix schema,create is-auth, send-reset-otp,reset-password

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name:{type:String, required: true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-    verifyOtp:{type:String,required:''},
-    verifyOtpExpireAt:{type:Number,default:0},
-    isAccountVerified:{type:Boolean,default:false},
-    verifyOtp: { type: String, default: '' },
-    reseOtpExpireAt:{type:Number,default:0}
-})
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    verifyOtp: { type: String, default: '' }, // หรือใส่ false หากไม่ต้องการบังคับ
+    verifyOtpExpireAt: { type: Number, default: 0 },
+    isAccountVerified: { type: Boolean, default: false },
+    resetOtp: { type: String, default: '' },
+    reseOtpExpireAt: { type: Number, default: 0 }
+});
 
-const userModel = mongoose.models.user|| mongoose.model('user',userSchema)
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
 
 export default userModel;
