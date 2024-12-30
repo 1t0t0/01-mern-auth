f3cff4597c65524fb5769b742b939957686b00cb
Toto <toto@Totos-MacBook-Pro.local>
2024-12-30 10:46:02 +0700

    add context provider

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
 
 const Login = () => {
 
     const navigate = useNavigate()
 
+    const { backendUrl, setIsLoggedin } = useContext(AppContext)
+
     const [state, setState] = useState('Sign Up')
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const onSubmitHandle = async (e) => {
+        try {
+            e.preventDefault()
+
+            if (state === 'Sign Up') {
+                await axios.post(backendUrl + '/api/auth/register')
+            } else {
+
+            }
+        } catch (error) {
+
+        }
+    }
 
     return (
         <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
@@ -19,7 +37,7 @@ const Login = () => {
                 <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create account' : 'Login'}</h2>
                 <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login to your account'}</p>
 
-                <form>
+                <form onSubmit={onSubmitHandle}>
                     {state === 'Sign Up' && (
                         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                             <img src={assets.person_icon} alt="" />
